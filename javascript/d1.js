e1ed738a0b1574d476529602e52e253e74788ee6
wzyskq <104765161+wzyskq@users.noreply.github.com>
2022-06-05 20:32:34 +0800

    Add files via upload
    coffee turn js

--- javascript/d1.js
+++ javascript/d1.js
@@ -1,3 +1,8 @@
-document.oncontextmenu = function(){
-    return false;
+function on(){
+    document.getElementById("coffeeshop").style.cssText = "background-color: rgba(0, 0, 0, 0.3); z-index:100";
+    document.getElementById("coffeecard").style.display= "block";
+}
+function off(){
+    document.getElementById("coffeeshop").style.cssText = "background-color: rgba(0, 0, 0, 0); z-index:-100";
+    document.getElementById("coffeecard").style.display= "none";
 }
